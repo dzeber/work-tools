9a845dc77229a01f3dadeaf6cefe150ab36fdce6
Dave Zeber <dzeber@mozilla.com>
2014-02-04 10:53:49 -0600

    Added order df function

--- R/R-tools.R
+++ R/R-tools.R
@@ -5,3 +5,14 @@
 ##############################################################
 
 
+## Order the rows of a data frame over one or more columns. 
+## Specify the df and the column names. 
+
+order.df = function(df, ..., decreasing=FALSE, na.last=TRUE) {
+    cols = list(...)
+    cols = lapply(cols, function(nc) { unlist(df[[nc]]) })
+    df = df[do.call(order, c(cols, decreasing=decreasing, na.last=na.last)),]
+    rownames(df) = NULL
+    df
+}
+
