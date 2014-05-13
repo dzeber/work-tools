a56ff1cbc7f71289b16429b0260b9fdc017f80e8
Dave Zeber <dzeber@mozilla.com>
2014-05-13 17:30:41 -0500

    Added option to attach env later in source2env

--- R/other/source-to-env.R
+++ R/other/source-to-env.R
@@ -7,18 +7,19 @@
 
 
 ## Creates a new environment with the specified name
-## and adds it to the search path if it is not already there. 
+## and adds it to the search path if it is not already there
+## or if attach.first is FALSE.
 ## 
 ## If specified path is a directory, sources all contained .R files.
 ## Otherwise sources path as file. Assigns contents to the environment.
 ##
 ## Argument keep.source is passed FALSE. 
 
-source2env <- function(src.path, env.name) {
-    if(env.name %in% search()) {
-        e <- as.environment(env.name)
+source2env <- function(src.path, env.name, attach.first = TRUE) {
+    e <- if(env.name %in% search()) {
+        as.environment(env.name)
     } else {
-        e <- attach(NULL, name = env.name)
+        if(attach.first) attach(NULL, name = env.name) else new.env()
     }
     if(file_test("-d", src.path)) {
         for(f in list.files(src.path, pattern = "\\.R$")) {
@@ -27,5 +28,6 @@ source2env <- function(src.path, env.name) {
     } else {
         sys.source(src.path, envir = e, keep.source = FALSE)
     }
+    if(!attach.first) attach(e, name = env.name)
     rm(e, f)
 }
