26c62cdd342daa1b46f5f57e6279ae774734d947
Dave Zeber <dzeber@mozilla.com>
2014-05-12 20:41:54 -0500

    Modified loader to load into environment

--- R/load-fhr-tools.R
+++ R/load-fhr-tools.R
@@ -1,12 +1,16 @@
 ##############################################################
 ### 
-###  Code to load FHR util functions. 
+###  Code to load FHR util functions to an environment 
+###  on the search path named "fhrtools".
 ###  Will eventually be turned into a package. 
 ### 
 ##############################################################
 
 
-for(f in list.files("fhr", pattern = "\\.R$")) {
-    source(file.path("fhr", f), keep.source = FALSE)
-}
-
+local({
+    fhr.dir <- file.path(dirname(sys.frame(1)$ofile), "fhr")
+    e <- attach(NULL, name = "fhrtools")
+    for(f in list.files(fhr.dir, pattern = "\\.R$")) {
+        sys.source(file.path(fhr.dir, f), envir = e, keep.source = FALSE)
+    }
+})
