e1b4121b9c2dadbc551d73da01e7fe09fca2c115
Dave Zeber <dzeber@mozilla.com>
2014-05-14 20:53:12 -0500

    Moved functions for working with payload fields to separate file

--- R/fhr/payload.R
+++ R/fhr/payload.R
@@ -13,5 +13,14 @@ get.val = function(d, n) { isn(d[n][[1]]) }
 
 
 
+## Check validity of strings intended to represent dates using regex. 
+## In FHR, the format should be yyyy-mm-dd.
+## Returns TRUE if all elements of input are correctly formatted, FALSE otherwise.
+
+valid.dates = function(d) {
+    ## Use regex to check for valid date format between 1900-01-01 and 2999-12-31.
+    all(grepl("^(19|2[0-9])\\d{2}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|02-(0[1-9]|[12][0-9]))$", d))
+}
+
 
 
