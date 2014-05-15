1be84c965cc605a34a8d507adaba81d939fd5f28
Dave Zeber <dzeber@mozilla.com>
2014-05-14 20:56:11 -0500

    Moved valid.dates to payload.R

--- R/fhr/query.R
+++ R/fhr/query.R
@@ -9,14 +9,10 @@
 #####
 #####  NB: TO USE THIS FILE ON HALA, DO NOT SOURCE DIRECTLY.
 #####  
-#####  INSTEAD, USE source("/usr/local/share/load-fhr-query.R")
+#####  INSTEAD, USE source("/usr/local/share/load-fhr-tools.R")
 #####
 #####################################################################
 
-
-#source("./fhr-tools.R")
-# if(!exists("wrap.fun"))
-    # source("../rhipe/rhipe-tools.R")
 
 
 ## Run query to extract FHR data from samples. 
@@ -81,11 +77,6 @@
 ## Outputs the job handle z, augmented with a few additional values for convenient access.
 ##    * z$input.data is a string representing the input dataset
 ##    * z$param is the final param list passed to the job
-##    * z$count is the number of records that were passed to the logic function after filtering
-##    * z$stats is the matrix of filtering counters collected in the map phase
-##    * z$end.state is the matrix of end-state counters collected through the ##        return value of the logic function
-##    * z$mapred is the matrix of relevant Map/Combine/Reduce record counts
-##
 
 fhr.query = function(output.folder = NULL
                     ,data.in = "1pct"
@@ -394,14 +385,8 @@ fhr.query = function(output.folder = NULL
     z
 }
 
-
-## Check validity of strings intended to represent dates using regex. 
-## In FHR, the format should be yyyy-mm-dd.
-## Returns TRUE if all elements of input are correctly formatted, FALSE otherwise.
-
-valid.dates = function(d) {
-    ## Use regex to check for valid date format between 1900-01-01 and 2999-12-31.
-    all(grepl("^(19|2[0-9])\\d{2}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|02-(0[1-9]|[12][0-9]))$", d))
-}
-
-
+##    * z$count is the number of records that were passed to the logic function after filtering
+##    * z$stats is the matrix of filtering counters collected in the map phase
+##    * z$end.state is the matrix of end-state counters collected through the ##        return value of the logic function
+##    * z$mapred is the matrix of relevant Map/Combine/Reduce record counts
+##
